48a85ce61da71a86ae510d842578901a8b714891
john <john4pius@gmail.com>
2020-07-12 06:22:14 +0100

    update test_runner.sh

--- backend/setup.sh
+++ backend/setup.sh
@@ -1,6 +1,4 @@
-DATABASE_URL
-AUTH0_DOMAIN
-ALGORITHMS
-API_AUDIENCE
-
 export DATABASE_URL=postgres://demorole1:password1@localhost:5432/movies
+
+export FLASK_APP=app
+export FLASK_DEBUG=True
---
+++ backend/test_runner.sh
@@ -0,0 +1,14 @@
+
+. setup.sh
+export ENV='test'
+echo 'DROPPING TABLES... (if any)'
+python manage.py db downgrade
+
+echo 'CREATING TABLES...'
+python manage.py db upgrade
+
+echo 'SEEDING TABLES...'
+python manage.py seed
+
+echo 'RUNNING TEST...'
+python3 test.py
