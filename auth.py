dcbea3c6401d30cac6ae1de3c6576fd35bb73901
john <john4pius@gmail.com>
2020-07-14 08:44:46 +0100

    fix authenticcation

--- auth.py
+++ auth.py
@@ -9,6 +9,7 @@ ALGORITHMS = ['RS256']
 API_AUDIENCE = 'moviescast'
 
 ''' AuthError Exception '''
+
 '''
 AuthError Exception
 A standardized way to communicate auth failure modes
@@ -71,24 +72,18 @@ def check_permissions(permission, payload):
 
 
 def verify_decode_jwt(token):
-    jsonurl = urlopen(f'https://{AUTH0_DOMAIN}/.well-known/jwks.json')
+    jsonurl = urlopen("https://"+AUTH0_DOMAIN+"/.well-known/jwks.json")
     jwks = json.loads(jsonurl.read())
     unverified_header = jwt.get_unverified_header(token)
     rsa_key = {}
-    if 'kid' not in unverified_header:
-        raise AuthError({
-            'code': 'invalid_header',
-            'description': 'Authorization malformed.'
-        }, 401)
-
-    for key in jwks['keys']:
-        if key['kid'] == unverified_header['kid']:
+    for key in jwks["keys"]:
+        if key["kid"] == unverified_header["kid"]:
             rsa_key = {
-                'kty': key['kty'],
-                'kid': key['kid'],
-                'use': key['use'],
-                'n': key['n'],
-                'e': key['e']
+                "kty": key["kty"],
+                "kid": key["kid"],
+                "use": key["use"],
+                "n": key["n"],
+                "e": key["e"]
             }
     if rsa_key:
         try:
@@ -97,45 +92,40 @@ def verify_decode_jwt(token):
                 rsa_key,
                 algorithms=ALGORITHMS,
                 audience=API_AUDIENCE,
-                issuer='https://' + AUTH0_DOMAIN + '/'
+                issuer="https://"+AUTH0_DOMAIN+"/"
             )
-
             return payload
-
         except jwt.ExpiredSignatureError:
-            raise AuthError({
-                'code': 'token_expired',
-                'description': 'Token expired.'
-            }, 401)
-
+            raise AuthError({"code": "token_expired",
+                             "description": "token is expired"}, 401)
         except jwt.JWTClaimsError:
-            raise AuthError({
-                'code': 'invalid_claims',
-                'description': 'Incorrect claims. Please, check the audience and issuer.'
-            }, 401)
+            raise AuthError({"code": "invalid_claims",
+                             "description":
+                             "incorrect claims,"
+                             "please check the audience and issuer"}, 401)
         except Exception:
-            raise AuthError({
-                'code': 'invalid_header',
-                'description': 'Unable to parse authentication token.'
-            }, 400)
-    raise AuthError({
-                'code': 'invalid_header',
-                'description': 'Unable to find the appropriate key.'
-            }, 400)
+            raise AuthError({"code": "invalid_header",
+                             "description":
+                             "Unable to parse authentication"
+                             " token."}, 401)
+
+        _request_ctx_stack.top.current_user = payload
+    raise AuthError({"code": "invalid_header",
+                     "description": "Unable to find appropriate key"}, 401)
 
 
 def requires_auth(permission=''):
-
     def requires_auth_decorator(f):
         @wraps(f)
         def wrapper(*args, **kwargs):
+            token = get_token_auth_header()
             try:
-                token = get_token_auth_header()
                 payload = verify_decode_jwt(token)
-                check_permissions(permission, payload)
-            except AuthError as err:
-                abort(401, err.error)
-
+            except:
+                raise AuthError({
+                                "code": "invalid_token",
+                                "description": "Invalid Token"}, 401)
+            check_permissions(permission, payload)
             return f(payload, *args, **kwargs)
 
         return wrapper
